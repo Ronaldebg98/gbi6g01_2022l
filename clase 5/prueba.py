ffb67aeba98df3ba47ff4e12071cfd3c682ada9f
Ronaldebg98 <ronald.bazantes@est.ikiam.edu.ec>
2022-05-27 15:37:50 -0500

    Cambios en la rama Ronald

--- clase 5/prueba.py
+++ clase 5/prueba.py
@@ -2,4 +2,4 @@
 # Código paera calcular el doble
 x = int(input())
 y = 2*x
-print ("El doble de ", str(x), "es " str(y))
+print ("El doble de ", x, "es " y)
---
+++ clase 5/prueba.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#Triple de un número
+x=$1
+y=$2
+z=$(( x*y ))
+echo "EL producto $x por $y es igual a $z"
+
